48477957736a615cf7404543ad03039c5c71d1dd
Cemre Acar <cemreacar94@gmail.com>
2023-01-28 18:51:57 +0300

    search added & mobile menu fixed

---
+++ components/features/Search.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import Image from 'next/image'
+import OutsideClickHandler from 'react-outside-click-handler'
+import { GiCancel } from "react-icons/gi";
+
+const Search = (props) => {
+    return (
+        <div className="fixed top-0 w-screen h-screen z-50 
+        after:content-[''] after:w-screen after:h-screen after:bg-white 
+        after:opacity-60 after:absolute after:top-0 grid place-content-center">
+            <OutsideClickHandler onOutsideClick={() => {
+                props.setStatus(false)
+            }}>
+                <div className="w-full h-full grid place-content-center">
+                    <div className="relative z-50 md:w-[600px] w-[370px] text-center h-[500px] p-10 bg-white border-2 rounded-2xl">
+                        <h1 className="extra-font text-black text-4xl mb-5">Search</h1>
+                        <input type="text" placeholder="Search" className="border w-full mt-5" />
+                        <ul className="mt-10 h-[250px] overflow-auto">
+                            <li className="flex items-center justify-between py-2 px-4 hover:bg-primary transtion-all">
+                                <div className="relative flex">
+                                    <Image src="/images/tshirt.jpeg" width={48} height={48} />
+                                </div>
+                                <span className="font-bold">Awesome T-Shirt</span>
+                                <span className="font-bold">$25</span>
+                            </li>
+
+                            <li className="flex items-center justify-between py-2 px-4 hover:bg-primary transtion-all">
+                                <div className="relative flex">
+                                    <Image src="/images/tshirt.jpeg" width={48} height={48} />
+                                </div>
+                                <span className="font-bold">Awesome T-Shirt</span>
+                                <span className="font-bold">$25</span>
+                            </li>
+
+                            <li className="flex items-center justify-between py-2 px-4 hover:bg-primary transtion-all">
+                                <div className="relative flex">
+                                    <Image src="/images/tshirt.jpeg" width={48} height={48} />
+                                </div>
+                                <span className="font-bold">Awesome T-Shirt</span>
+                                <span className="font-bold">$25</span>
+                            </li>
+
+                            <li className="flex items-center justify-between py-2 px-4 hover:bg-primary transtion-all">
+                                <div className="relative flex">
+                                    <Image src="/images/tshirt.jpeg" width={48} height={48} />
+                                </div>
+                                <span className="font-bold">Awesome T-Shirt</span>
+                                <span className="font-bold">$25</span>
+                            </li>
+
+                            <li className="flex items-center justify-between py-2 px-4 hover:bg-primary transtion-all">
+                                <div className="relative flex">
+                                    <Image src="/images/tshirt.jpeg" width={48} height={48} />
+                                </div>
+                                <span className="font-bold">Awesome T-Shirt</span>
+                                <span className="font-bold">$25</span>
+                            </li>
+
+                            <li className="flex items-center justify-between py-2 px-4 hover:bg-primary transtion-all">
+                                <div className="relative flex">
+                                    <Image src="/images/tshirt.jpeg" width={48} height={48} />
+                                </div>
+                                <span className="font-bold">Awesome T-Shirt</span>
+                                <span className="font-bold">$25</span>
+                            </li>
+                        </ul>
+                        <button className="absolute top-6 right-6" onClick={() => { props.setStatus(false) }}>
+                            <GiCancel size={25} className="hover:text-primary transition-all" />
+                        </button>
+                    </div>
+                </div>
+            </OutsideClickHandler>
+        </div>
+    )
+}
+
+export default Search
--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,72 +1,71 @@
 import Logo from "../features/Logo";
-import { FaUserAlt } from "react-icons/fa";
+import { FaCartPlus, FaUserAlt } from "react-icons/fa";
+import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { HiShoppingCart } from "react-icons/hi";
+import OutsideClickHandler from "react-outside-click-handler";
+import Search from "../features/Search";
+import { GiCancel, GiHamburgerMenu } from "react-icons/gi";
 
 const Header = () => {
-    return (
-        <div className="p-[1rem] bg-secondary text-white">
-            <div className="flex container mx-auto items-center justify-between">
-                <div className="hidden md:flex">
-                    <Logo />
-                </div>
-                <div className="w-full md:w-auto">
-                    <nav className="w-full flex-col justify-around md:flex text-center">
-                        <div className="md:hidden">
-                            <Logo />
-                        </div>
-                        <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center px-[0.8rem] py-[0.4rem] mt-3 mb-3 md:mt-0 md:mb-0 text-sm text-white hover:text-black rounded-lg md:hidden border-2 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-secondary dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                            <span className="sr-only">Open Menu</span>
-                            <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
-                        </button>
-                        <div className="hidden w-full md:flex md:w-auto" id="navbar-default">
-                            <ul className="w-full md:flex flex-col md:flex-row gap-x-1 justify-between text-center mt-4 md:mt-0">
-                                <li className="uppercase">
-                                    <a href="" className="block px-[0.7rem] py-[0.5rem] text-md hover:text-black transition">HOME</a>
-                                </li>
-                                <li className="uppercase">
-                                    <a href="" className="block px-[0.7rem] py-[0.5rem] text-md hover:text-black transition">PRODUCTS</a>
-                                </li>
-                                <li className="uppercase">
-                                    <a href="" className="block px-[0.7rem] py-[0.5rem] text-md hover:text-black transition">ABOUT</a>
-                                </li>
-                                <div className="flex md:hidden justify-center pt-7 md:justify-between items-center gap-x-1">
-                                    <a href="#" className="p-[0.5rem] hover:text-black">
-                                        <FaUserAlt />
-                                    </a>
-                                    <a href="#" className="p-[0.5rem] hover:text-black">
-                                        <FaSearch />
-                                    </a>
-                                    <a href="#">
-                                        <div className="border-2 ml-2 px-[0.8rem] py-[0.4rem] text-md rounded-md flex items-center text-center hover:text-black hover:bg-white hover:text-black">
-                                            <span className="text-lg"><HiShoppingCart /></span>
-                                            <span className="pl-2 uppercase text-sm">CART</span>
-                                        </div>
-                                    </a>
-                                </div>
-                            </ul>
+    const [isSearchModal, setIsSearchModal] = useState(false)
+    const [isMenuModal, setIsMenuModal] = useState(false);
 
-                        </div>
+    return (
+        <>
+            <div className="p-[1rem] bg-secondary text-white">
+                <div className="flex container mx-auto items-center justify-between">
+                    <div className="flex">
+                        <Logo />
+                    </div>
+
+                    <nav className={`sm:static absolute top-0 left-0 sm:w-auto sm:h-auto w-full h-full sm:text-white text-black sm:bg-transparent bg-white sm:flex hidden ${isMenuModal === true && "!grid place-content-center"}`}>
+                        <ul className="flex gap-x-2 sm:flex-row flex-col items-center">
+                            <li className="uppercase">
+                                <a href="" className="block px-[0.7rem] py-[0.5rem] text-md hover:text-black transition-all">HOME</a>
+                            </li>
+                            <li className="uppercase">
+                                <a href="" className="block px-[0.7rem] py-[0.5rem] text-md hover:text-black transition-all">PRODUCTS</a>
+                            </li>
+                            <li className="uppercase">
+                                <a href="" className="block px-[0.7rem] py-[0.5rem] text-md hover:text-black transition-all">ABOUT</a>
+                            </li>
+                        </ul>
+                        {isMenuModal && (
+                            <button
+                                className="absolute  top-4 right-4 z-50"
+                                onClick={() => setIsMenuModal(false)}>
+                                <GiCancel size={25} className=" transition-all" />
+                            </button>
+                        )}
                     </nav>
-                </div>
-                <div className="hidden md:flex justify-between items-center gap-x-1">
-                    <a href="#" className="p-[0.5rem] hover:text-black">
-                        <FaUserAlt />
-                    </a>
-                    <a href="#" className="p-[0.5rem] hover:text-black">
-                        <FaSearch />
-                    </a>
-                    <a href="#">
-                        <div className="border-2 ml-2 px-[0.8rem] py-[0.4rem] text-md rounded-md flex items-center text-center hover:text-black hover:bg-white hover:text-black">
-                            <span className="text-lg"><HiShoppingCart /></span>
-                            <span className="pl-2 uppercase text-sm">CART</span>
-                        </div>
-                    </a>
+
+                    <div className="flex justify-between items-center gap-x-1">
+                        <a href="#" className="p-[0.5rem] hover:text-black">
+                            <FaUserAlt className="transition-all"/>
+                        </a>
+                        <button className="p-[0.5rem] hover:text-black" onClick={() => setIsSearchModal(true)}>
+                            <FaSearch className="transition-all"/>
+                        </button>
+                        <a href="#" className="text-xl p-[0.5rem] hover:text-black">
+                            <HiShoppingCart className="transition-all"/>
+                        </a>
+                        <button className="sm:hidden inline-block p-[0.5rem] hover:text-black"
+                            onClick={() => setIsMenuModal(true)}>
+                            <GiHamburgerMenu className="text-xl transition-all" />
+                        </button>
+                    </div>
                 </div>
             </div>
 
+            {
+                isSearchModal &&
+                <Search setStatus={setIsSearchModal} />
+            }
+        </>
 
-        </div>
+
+
     )
 }
 
